e1c744208d7e819d47802724267cdb0725126c2a
Edwin Tang <edwintang10@gmail.com>
2024-01-18 18:08:40 +0800

    feat(main): add init and controller constructor to inject elements programatically.

---
+++ src/main/java/software/fullstack/jfileexplorer/Directory.java
@@ -0,0 +1,4 @@
+package software.fullstack.jfileexplorer;
+
+public class Directory {
+}
--- src/main/java/software/fullstack/jfileexplorer/MainController.java
+++ src/main/java/software/fullstack/jfileexplorer/MainController.java
@@ -1,14 +1,44 @@
 package software.fullstack.jfileexplorer;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private Label notificationText;
+
+    /**
+     * Contains other columns
+     */
+    @FXML
+    private TableView directoryContentHolder;
 
     @FXML
+    private BorderPane root;
+
+    private Node emptyContent;
+
+    public MainController() {
+        Label emptyLabel = new Label();
+        emptyLabel.setText("Nothing to show here...");
+        HBox emptyHBox = new HBox(emptyLabel);
+        emptyHBox.setAlignment(Pos.CENTER);
+        emptyHBox.setStyle("-fx-background-color: white");
+
+        emptyContent=emptyHBox;
+    }
+
+    @FXML
+    public void initialize() {
+        root.setCenter(emptyContent);
+    }
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        notificationText.setText("Welcome to JavaFX Application!");
     }
 }
