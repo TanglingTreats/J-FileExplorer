691c25522a640ffa97810b9f8040d54cb69990d4
Edwin Tang <edwintang10@gmail.com>
2024-01-18 18:32:29 +0800

    feat(main): Add table view creation into controller constructor. Add method for 'New' button in tool bar

--- src/main/java/software/fullstack/jfileexplorer/MainController.java
+++ src/main/java/software/fullstack/jfileexplorer/MainController.java
@@ -3,10 +3,14 @@ package software.fullstack.jfileexplorer;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+
+import static software.fullstack.jfileexplorer.JFileExplorer.appWidth;
 
 public class MainController {
     @FXML
@@ -21,7 +25,12 @@ public class MainController {
     @FXML
     private BorderPane root;
 
+    /**
+     * Empty content notification
+     */
     private Node emptyContent;
+
+    private Node fileDirectoryView;
 
     public MainController() {
         Label emptyLabel = new Label();
@@ -31,14 +40,36 @@ public class MainController {
         emptyHBox.setStyle("-fx-background-color: white");
 
         emptyContent=emptyHBox;
+
+        // Initialize table view
+        TableView directoryView = new TableView();
+        TableColumn fileNameCol = new TableColumn("Name");
+        TableColumn fileSizeCol = new TableColumn("Size");
+
+        directoryView.getColumns().addAll(fileNameCol, fileSizeCol);
+        fileDirectoryView = directoryView;
     }
 
     @FXML
     public void initialize() {
+        // TODO: Set center content based on existence of content in folder
         root.setCenter(emptyContent);
     }
     @FXML
     protected void onHelloButtonClick() {
         notificationText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void createNewFile() {
+        System.out.println("Creating new file");
+        /*
+        Create editable row in file explorer.
+        Focus on name column
+        On enter,
+            confirm file creation
+        Else,
+            remove editable row from table view
+         */
     }
 }
