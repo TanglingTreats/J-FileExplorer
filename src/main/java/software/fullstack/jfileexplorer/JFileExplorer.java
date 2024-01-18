a1b167f7f145bf32d97d24f47d0566d38c3735b1
Edwin Tang <edwintang10@gmail.com>
2024-01-18 12:08:40 +0800

    chore(setup): rename files and scaffold code.

--- src/main/java/software/fullstack/jfileexplorer/JFileExplorer.java
+++ src/main/java/software/fullstack/jfileexplorer/JFileExplorer.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class JFileExplorer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(JFileExplorer.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 768);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        stage.setTitle("J File Explorer");
         stage.setScene(scene);
         stage.show();
     }
